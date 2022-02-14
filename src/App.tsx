8a66b3fbd9a453834a508d9b813d48604056c59f
quipcode <abdulkarim8ahmed@gmail.com>
2022-02-14 15:55:01 -0800

    most of the original pie chart came from eli elad elrom's blog post see here https://medium.com/react-courses/integrate-a-donut-chart-with-polylines-and-labels-utilizing-d3js-reactjs-and-typescript-2d23b8ffb108

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DonutChartWidget from './DonutChart/DonutChartWidget';
+import DonutChartWidget from './DonutCharts/DonutChartWidget';
 function App() {
   return (
     <div className="App">
